7c309a7a8946a0ccf92b1cf3000751f08752882b
Hadyan Ahmad <hadyandev@gmail.com>
2021-10-31 05:54:39 +0700

    set display qurabn public

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\NoteController;
+use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TagController;
-use App\Http\Controllers\NoteController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,14 +19,14 @@ use App\Http\Controllers\NoteController;
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::get('/', function() {
-    if(!is_null(Auth::user())) {
-        return redirect(route('dashboard'));
-    } else {
-        return view('auth.login');
-    }
-});
-
+// Route::get('/', function() {
+//     if(!is_null(Auth::user())) {
+//         return redirect(route('dashboard'));
+//     } else {
+//         return view('auth.login');
+//     }
+// });
+Route::get('/', [DashboardController::class, 'index']);
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
